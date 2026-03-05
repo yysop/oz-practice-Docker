00dfc8fd15a93290f1410e0e6ed2ddb4459d3b67
yysop <sysysy3142@gmail.com>
2026-03-05 15:04:33 +0900

    basic Doker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.13-slim
+WORKDIR /app
+# 현재 경로에 있는 모든 파일을 Container 안으로 모두 복사
+COPY . .
+# requirements.txt에 있는 것들 한 번에 설치 
+RUN pip install -r requirements.txt 
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from sqlalchemy import text
+
+from database import SessionFactory
+
+app = FastAPI()
+
+@app.get("/users")
+async def get_users_handler():
+    with SessionFactory() as session:
+        result = session.execute(
+            text("SELECT * FROM user;")
+        ).mappings().all()
+
+
+    return {"result": result}
