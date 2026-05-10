5144a6f3779f846a58efe36f2786eca781ee802c
yysop <sysysy3142@gmail.com>
2026-05-11 06:41:31 +0900

    Implement Redis-based chat handler for question processing
    Refactor chat handler to use Redis for job management and response retrieval.

--- api/main.py
+++ api/main.py
@@ -1,25 +1,56 @@
-# 0305
+import json
+import uuid
 
-from fastapi import FastAPI
-from sqlalchemy import text
+import redis import asyncio as aredis
+from fastapi import FastAPI, Body, Query, Path
 
-from database import SessionFactory # api.database 로 자동 수정된 거 삭제해줌
 
+# 답변 생성을 요청하면, 대기 발생
+# 대기하는 동안, 다른 일(HTTP 요청) 처리
+
+# responses
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True)
 
 
 app = FastAPI()
 
-# root_handler 추가
-@app.get("/")
-async def root_handler():
-    return {"ping": "pong247"}
+# [ 쿼리 파라미터(QueryParameter) ]
+# GET google.com/search?q=python -> 새로운 데이터를 만들어내거나, 데이터 변경 X
 
-@app.get("/users")
-async def get_users_handler():
-    with SessionFactory() as session:
-        result = session.execute(
-            text("SELECT * FROM user;")
-        ).mappings().all()
+# GET -> 새로운 데이터를 만들어내거나, 데이터 변경 X
+# POST -> 새로운 데이터를 생성
 
+# [1] 클라이언트에서 질문(question)을 요청한다.
+@app.post("/chats")
+async def chat_handler(
+    # RequestBody
+    # 3-1) 
+    # UserSignUpRequest = Body(...)
+    # 3-2)
+    question: str = Body(..., embed=True),
+    
+    # Query
+    # question: str = Query(...),
 
+    # Path
+    # question: str = Path(...),
+):
+    # [2] 결과 채널을 구독
+    job_id = str(uuid.uuid4())  # 작업을 식별할 수 있는 랜덤 식별자 발급
+    channel = f"result:{job_id}"
+
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
+
+    # [3] 답변 생성 작업 Enqueue
+    job = {"id": job_id, "question": question}
+    await redis_client.lpush("inference_queue", json.dumps(job))
+
+    # [4] 답변 생성 결과를 돌려받기
+    result = None
+    async for message in pubsub.listen():
+        if message["type"] == "message":
+            result = message["data"]
+            break
+        
     return {"result": result}
